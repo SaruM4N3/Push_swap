73725af864cf0c54df15f1c40bb06fef128d7c10
saruM4N3 <zakobajo@gmail.com>
2025-02-08 04:55:55 +0100

    done nothing except double chained list

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,15 +6,20 @@
 /*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 06:02:12 by zsonie            #+#    #+#             */
-/*   Updated: 2025/02/07 06:03:26 by zsonie           ###   ########.fr       */
+/*   Updated: 2025/02/08 03:47:46 by zsonie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+typedef struct s_stack
+{
+	int	value;
+	struct node *p_next;
+	struct node *p_previous;
+}	t_stack;
+
 
 #ifndef PUSH_SWAP_H
  #define PUSH_SWAP_H
 
 
-#endif // PUSH_SWAP_H
-
-
-
+#endif 
